015f46cfcc5c747e46933c409e2fe738bf81be84
Kevin Varchola <kevin.keko321@gmail.com>
2024-12-17 13:20:24 +0100

    Reimplemented consts

--- server/const.go
+++ server/const.go
@@ -1,7 +1,23 @@
 package main
 
+// Constants for the server connection
 const (
-	CONN_ADDRESS = "0.0.0.0"
-	CONN_PORT    = "8080"
-	CONN_NETWORK = "tcp"
+	CONN_ADDRESS = "0.0.0.0" // Address to bind the server
+	CONN_PORT    = "8080"    // Port to bind the server
+	CONN_NETWORK = "tcp"     // Network protocol to use
+)
+
+// Constants for the game settings
+const (
+	MAX_PLAYERS           = 2        // Maximum number of players in the game
+	DEFAULT_HEALTH        = 3        // Default health for each player
+	DEFAULT_AMMO          = 1        // Default ammo for each player
+	DEFAULT_ACTION        = "COVER"  // Default action for each player
+	ACTION_SHOOT          = "SHOOT"  // Action to shoot the opponent
+	ACTION_COVER          = "COVER"  // Action to cover and protect oneself
+	ACTION_RELOAD         = "RELOAD" // Action to reload the weapon
+	ACTION_TIMEOUT        = 20       // Timeout for player actions in seconds
+	ACTION_CHECK_INTERVAL = 500      // Interval to check player actions in milliseconds
+	ROUND_PAUSE_TIME      = 5        // Pause time between rounds in seconds
+	PING_TIME             = 30       // Time interval to ping clients in seconds
 )
