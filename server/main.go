9951eaedde58da69c76173f69ba546dd8382194a
Kevin Varchola <kevin.keko321@gmail.com>
2024-12-17 13:20:31 +0100

    Reimplemented main

--- server/main.go
+++ server/main.go
@@ -1,47 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"net"
 	"os"
 )
 
 func main() {
-	server := &GameServer{
-		rooms: make([]*Room, 0),
-	}
-
-	var address string
-
-	// Read arguments from the command line
-	args := os.Args
-	if len(args) >= 2 {
-		address = args[1]
-	} else {
-		address = CONN_ADDRESS + ":" + CONN_PORT
-	}
-
-	// Listen for incoming connections
-	listener, err := net.Listen(CONN_NETWORK, address)
-	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		return
-	}
+	address := get_address()
+	listener := start_server(address)
 	defer listener.Close()
 
-	fmt.Println("Starting server on", address)
+	accept_connections(listener)
+}
 
-	// Accept connections in a loop
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err.Error())
-			return
-		}
-
-		fmt.Println("Accepted connection from", conn.RemoteAddr().String())
-
-		client := &Client{conn: conn}
-		go server.Handle_client(client)
+func get_address() string {
+	args := os.Args
+	if len(args) >= 2 {
+		return args[1]
 	}
+	return CONN_ADDRESS + ":" + CONN_PORT
 }
