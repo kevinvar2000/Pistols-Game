927e6a53bee7689c23f86d427c6ab8a65ba301c2
Kevin Varchola <kevin.keko321@gmail.com>
2024-12-19 14:14:44 +0100

    added consts for client and gui

---
+++ client/const.py
@@ -0,0 +1,11 @@
+SERVER_IP = '127.0.0.1'  # Change to your server's IP
+SERVER_PORT = 8080       # Port the server is listening on
+BUFFER_SIZE = 1024       # Max size of data from the server
+
+# Window constants
+WIN_SIZE = "400x400"                 # Default window size
+TITLE_FONT = ("Arial", 16)           # Default font style
+BUTTON_FONT = ("Arial", 14)          # Default font style
+LABEL_FONT = ("Arial", 14)           # Default font style
+INPUT_FONT = ("Arial", 14)           # Default font style
+ERROR_FONT = ("Arial", 12)           # Default font style
