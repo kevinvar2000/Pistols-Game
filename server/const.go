54cbc8766f16c252d0d7a3c5aac6a19f7eb09fd2
Kevin Varchola <kevin.keko321@gmail.com>
2024-12-19 14:17:00 +0100

    Rename timeout const

--- server/const.go
+++ server/const.go
@@ -19,5 +19,5 @@ const (
 	ACTION_TIMEOUT        = 20       // Timeout for player actions in seconds
 	ACTION_CHECK_INTERVAL = 500      // Interval to check player actions in milliseconds
 	ROUND_PAUSE_TIME      = 5        // Pause time between rounds in seconds
-	PING_TIME             = 30       // Time interval to ping clients in seconds
+	READ_TIMEOUT          = 20       // Time interval to ping clients in seconds
 )
