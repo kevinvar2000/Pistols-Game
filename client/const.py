ebedc756dbf592bb874241841c6f5ab29a212892
Kevin Varchola <kevin.keko321@gmail.com>
2025-01-03 13:49:54 +0100

    Client Consts update

--- client/const.py
+++ client/const.py
@@ -1,8 +1,8 @@
 SERVER_IP = '127.0.0.1'  # Change to your server's IP
 SERVER_PORT = 8080       # Port the server is listening on
 BUFFER_SIZE = 1024       # Max size of data from the server
-PING_INTERVAL = 5        # Time in seconds between pings
-REQUEST_INTERVAL = 5     # Time in seconds between requests
+PING_INTERVAL = 3        # Time in seconds between pings
+REQUEST_INTERVAL = 1000  # Time in milliseconds between requests
 
 # Window constants
 WIN_SIZE = "400x400"                 # Default window size
