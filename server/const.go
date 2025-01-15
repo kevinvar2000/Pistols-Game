bd4540dd311baf4bbeda63a20e60065881739ab2
Kevin Varchola <kevin.keko321@gmail.com>
2025-01-15 19:44:47 +0100

    Added max player name len

--- server/const.go
+++ server/const.go
@@ -9,17 +9,18 @@ const (
 
 // Constants for the game settings
 const (
-	MAX_PLAYERS           = 2        // Maximum number of players in the game
-	DEFAULT_HEALTH        = 3        // Default health for each player
-	DEFAULT_AMMO          = 1        // Default ammo for each player
-	DEFAULT_ACTION        = "COVER"  // Default action for each player
-	ACTION_SHOOT          = "SHOOT"  // Action to shoot the opponent
-	ACTION_COVER          = "COVER"  // Action to cover and protect oneself
-	ACTION_RELOAD         = "RELOAD" // Action to reload the weapon
-	ACTION_TIMEOUT        = 20       // Timeout for player actions in seconds
-	ACTION_CHECK_INTERVAL = 500      // Interval to check player actions in milliseconds
-	ROUND_WAIT_TIME       = 1        // Pause time between rounds in seconds
-	READ_TIMEOUT          = 20       // Time interval to ping clients in seconds
-	READ_DEADLINE         = 30       // Deadline for reading from clients in seconds
-	RECONNECT_TIMEOUT     = 30       // Timeout for client reconnection in seconds
+	MAX_PLAYER_NAME_LENGTH = 20       // Maximum length of the player name
+	MAX_PLAYERS            = 2        // Maximum number of players in the game
+	DEFAULT_HEALTH         = 3        // Default health for each player
+	DEFAULT_AMMO           = 1        // Default ammo for each player
+	DEFAULT_ACTION         = "COVER"  // Default action for each player
+	ACTION_SHOOT           = "SHOOT"  // Action to shoot the opponent
+	ACTION_COVER           = "COVER"  // Action to cover and protect oneself
+	ACTION_RELOAD          = "RELOAD" // Action to reload the weapon
+	ACTION_TIMEOUT         = 20       // Timeout for player actions in seconds
+	ACTION_CHECK_INTERVAL  = 500      // Interval to check player actions in milliseconds
+	ROUND_WAIT_TIME        = 1        // Pause time between rounds in seconds
+	READ_TIMEOUT           = 20       // Time interval to ping clients in seconds
+	READ_DEADLINE          = 30       // Deadline for reading from clients in seconds
+	RECONNECT_TIMEOUT      = 30       // Timeout for client reconnection in seconds
 )
